3360b2a267c307db4df78a7fbed0a031e3532d7a
limitimil <zlsh10340@gmail.com>
2021-10-30 21:39:33 -0700

    ROUTER: poc add new page

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/board',
+    name: 'BulletinBoard',
+    component: () => import(/* webpackChunkName: "board" */ '../components/BulletinBoard/Editor.vue'),
+  },
 ]
 
 const router = createRouter({
