00e0d064acf24c8008efcfdaed8d5b84b6f80255
limitimil <zlsh10340@gmail.com>
2021-10-30 21:40:22 -0700

    VUE: bulletin board

---
+++ src/components/BulletinBoard/Editor.vue
@@ -0,0 +1,34 @@
+<template>
+  <div> Hello World </div>
+</template>
+<script lang="ts">
+
+export default {
+  name: 'New-Component',
+  props: [],
+  components: {
+  },
+  data() {
+    return {
+      helloworold: 'helloworld' as string,
+    }
+  },
+}
+</script>
+<style scoped lang="less">
+.hack1{
+ background-color: hsla(208, 26%, 75%, 1);
+}
+.hack2{
+ background-color: hsla(17, 25%, 33%, 1);
+}
+.hack3{
+ background-color: hsla(203, 16%, 96%, 1);
+}
+.hack4{
+ background-color: hsla(214, 15%, 62%, 1);
+}
+.hack5{
+ background-color: hsla(240, 7%, 47%, 1);
+}
+</style>
