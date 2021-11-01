d3d7292b02734f877d9f660ca50cc66be4dca6c1
limitimil <zlsh10340@gmail.com>
2021-11-01 07:14:06 -0700

    3 files: align to best practice

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Quasar } from 'quasar'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import quasarUserOptions from './quasar-user-options'
+import quasarUserOptions from '@/plugins/quasar-user-options'
 
 createApp(App).use(Quasar, quasarUserOptions).use(store).use(router)
   .mount('#app')
