58f1229eddbd1f7f0697ee37a43efe4a011ac3b0
limitimil <zlsh10340@gmail.com>
2021-11-06 16:56:19 +0800

    MAIN: install cookie-next

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { Quasar } from 'quasar'
+import { VueCookieNext } from 'vue-cookie-next'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import quasarUserOptions from '@/plugins/quasar-user-options'
 
 createApp(App).use(Quasar, quasarUserOptions).use(store).use(router)
+  .use(VueCookieNext)
   .mount('#app')
