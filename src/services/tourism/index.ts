9c02ae2c5729ce50deb05de10ea14bc2c6fa2e64
limitimil <zlsh10340@gmail.com>
2021-11-09 07:38:00 -0800

    Service: support offset

--- src/services/tourism/index.ts
+++ src/services/tourism/index.ts
@@ -1,9 +1,10 @@
 import Tourism from '@/data-fetch/tourism'
 
 const dataFetcher = new Tourism()
+const calcSkip = (count: number, offset: number) => offset * count
 export default class TourismService {
   // eslint-disable-next-line class-methods-use-this
-  public async top(count: number): Promise<any> {
-    return dataFetcher.getScenicSpot({ $top: count || 30 })
+  public async top(count: number, offset: number): Promise<any> {
+    return dataFetcher.getScenicSpot({ $top: count || 30, $skip: calcSkip(count, offset) })
   }
 }
