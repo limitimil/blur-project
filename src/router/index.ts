3fed39a9ecbf19f89179c4df10504fc89b158885
limitimil <zlsh10340@gmail.com>
2021-11-09 07:10:52 -0800

    ROUTER: add data story routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,6 +39,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/HelloWorld.vue'),
   },
+  {
+    path: '/data-stories/:storyId',
+    name: 'DataStories',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/dataStories/StoryRoot.vue'),
+  },
 ]
 
 const router = createRouter({
