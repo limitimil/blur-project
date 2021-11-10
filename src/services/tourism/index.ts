5d32cdc1b15368d69e36242247dfa0440bf17d79
limitimil <zlsh10340@gmail.com>
2021-11-10 05:46:43 -0800

    remove eslint comment

--- src/services/tourism/index.ts
+++ src/services/tourism/index.ts
@@ -3,7 +3,6 @@ import Tourism from '@/data-fetch/tourism'
 const dataFetcher = new Tourism()
 const calcSkip = (count: number, offset: number) => offset * count
 export default class TourismService {
-  // eslint-disable-next-line class-methods-use-this
   public async top(count: number, offset: number): Promise<any> {
     return dataFetcher.getScenicSpot({ $top: count || 30, $skip: calcSkip(count, offset) })
   }
