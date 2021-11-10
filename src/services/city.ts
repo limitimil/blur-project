2aaf4d36a93298b59a01f000f9e062d087e0fdab
limitimil <zlsh10340@gmail.com>
2021-11-10 06:57:15 -0800

    Tourism Service
    SERVICE: apply command pattern
    SERVICE: fetche use builder pattern
    SERVICE: builder pattern should always new before build
    
    SERVICES : search with city
    SERVICE: city
    SERVICE: get by keyword
    SERVICE; net class name
    SERVICE: useCollection

---
+++ src/services/city.ts
@@ -0,0 +1,95 @@
+export default class CityService {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async getCities(): Promise<any> {
+    return [
+      {
+        name: '臺北市',
+        key: 'Taipei',
+      },
+      {
+        name: '新北市',
+        key: 'NewTaipei',
+      },
+      {
+        name: '桃園市',
+        key: 'Taoyuan',
+      },
+      {
+        name: '臺中市',
+        key: 'Taichung',
+      },
+      {
+        name: '臺南市',
+        key: 'Tainan',
+      },
+      {
+        name: '高雄市',
+        key: 'Kaohsiung',
+      },
+      {
+        name: '基隆市',
+        key: 'Keelung',
+      },
+      {
+        name: '新竹市',
+        key: 'Hsinchu',
+      },
+      {
+        name: '新竹縣',
+        key: 'HsinchuCounty',
+      },
+      {
+        name: '苗栗縣',
+        key: 'MiaoliCounty',
+      },
+      {
+        name: '彰化縣',
+        key: 'ChanghuaCounty',
+      },
+      {
+        name: '南投縣',
+        key: 'NantouCounty',
+      },
+      {
+        name: '雲林縣',
+        key: 'YunlinCounty',
+      },
+      {
+        name: '嘉義縣',
+        key: 'ChiayiCounty',
+      },
+      {
+        name: '嘉義市',
+        key: 'Chiayi',
+      },
+      {
+        name: '屏東縣',
+        key: 'PingtungCounty',
+      },
+      {
+        name: '宜蘭縣',
+        key: 'YilanCounty',
+      },
+      {
+        name: '花蓮縣',
+        key: 'HualienCounty',
+      },
+      {
+        name: '臺東縣',
+        key: 'TaitungCounty',
+      },
+      {
+        name: '金門縣',
+        key: 'KinmenCounty',
+      },
+      {
+        name: '澎湖縣',
+        key: 'PenghuCounty',
+      },
+      {
+        name: '連江縣',
+        key: 'LienchiangCounty',
+      },
+    ]
+  }
+}
--- src/services/tourism/index.ts
+++ src/services/tourism/index.ts
@@ -1,9 +1,75 @@
 import Tourism from '@/data-fetch/tourism'
+import LocalCollection from '@/data-fetch/localCollection'
 
-const dataFetcher = new Tourism()
 const calcSkip = (count: number, offset: number) => offset * count
 export default class TourismService {
-  public async top(count: number, offset: number): Promise<any> {
-    return dataFetcher.getScenicSpot({ $top: count || 30, $skip: calcSkip(count, offset) })
+  private count: number = 30;
+
+  private offset: number = 0;
+
+  private city: string | undefined = undefined;
+
+  private keyword: string | undefined= undefined;
+
+  private className: string | undefined= undefined;
+
+  private isCollection: boolean = false;
+
+  public async fetch(): Promise<any> {
+    const arg = { $top: this.count, $skip: calcSkip(this.count, this.offset) }
+    let builder = new Tourism().withArg(arg)
+    if (this.isCollection) {
+      const collectedIds = new LocalCollection().getCollectedSceneSpotIds()
+      console.log(collectedIds)
+      if (collectedIds.length) {
+        builder = builder.withSubset(collectedIds)
+      } else {
+        return []
+      }
+    }
+    if (this.className) {
+      builder = builder.withClassName(this.className)
+    }
+    if (this.city) {
+      builder = builder.withCity(this.city)
+    }
+    if (this.keyword) {
+      builder = builder.withKeyword(this.keyword)
+    }
+    return builder.getScenicSpotInvoker()
+  }
+
+  public async clean(): Promise<any> {
+    this.city = undefined
+    this.keyword = undefined
+    this.className = undefined
+    this.count = 30
+    this.offset = 0
+    return this.fetch()
+  }
+
+  public setCity(city: string): void {
+    this.city = city
+  }
+
+  public setClassName(className: string): void {
+    this.className = className
+  }
+
+  public top(count: number): void {
+    this.count = count
+    this.offset = 0
+  }
+
+  public byKeyword(keyword: string): void {
+    this.keyword = keyword
+  }
+
+  public toPage(offset: number): void {
+    this.offset = offset
+  }
+
+  public useCollection(value: boolean): void {
+    this.isCollection = value
   }
 }
