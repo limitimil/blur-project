84259294036f3ce1ec3359902893f8f04550725c
Uren You <urenyou@cybersoft4u.com>
2021-11-11 12:37:38 +0800

    [Travel] feat: new view for travel

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,14 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
@@ -21,7 +17,7 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/HelloWorld.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Travel.vue'),
   },
   {
     path: '/week2',
