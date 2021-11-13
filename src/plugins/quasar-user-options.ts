a29a5e97be71c9235587fdacb1b3a003bd8123ed
limitimil <zlsh10340@gmail.com>
2021-11-12 21:10:26 -0800

    QUASAR: fix import

--- src/plugins/quasar-user-options.ts
+++ src/plugins/quasar-user-options.ts
@@ -2,10 +2,7 @@ import 'quasar/dist/quasar.css'
 import '@/styles/quasar.scss'
 import lang from 'quasar/lang/zh-TW.js'
 import '@quasar/extras/material-icons/material-icons.css'
-import {
-  Cookies,
-  LocalStorage,
-} from 'quasar'
+import { Cookies, LocalStorage } from 'quasar'
 
 // To be used on app.use(Quasar, { ... })
 export default {
