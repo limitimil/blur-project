fd4be73289c0f14644e8041cbf3fe9b154881e29
limitimil <zlsh10340@gmail.com>
2021-11-13 06:00:38 -0800

    2 files: install travel store

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { Quasar } from 'quasar'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import travelStore from './store/travel'
 import quasarUserOptions from '@/plugins/quasar-user-options'
 
 createApp(App).use(Quasar, quasarUserOptions).use(store).use(router)
+  .use(travelStore)
   .mount('#app')
