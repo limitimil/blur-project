5f7250e79f66be337809136fc98d5d9fbf76adee
limitimil <zlsh10340@gmail.com>
2021-11-14 04:38:26 -0800

    Component Story:

---
+++ src/components/componentStories/StoryRoot.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h4>{{ storyId }}</h4>
+      <br />
+      <div>Hello World</div>
+      <TaiwanMap/>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+import TaiwanMap from '@/components/taiwanMap/index.vue'
+
+export default {
+  name: 'ComponentStory',
+  components: {
+    TaiwanMap,
+  },
+  setup() {
+    const route = useRoute()
+
+    const storyId = ref(route.params.storyId)
+
+    return {
+      storyId,
+    }
+  },
+}
+</script>
