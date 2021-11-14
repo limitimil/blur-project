f7d45e0dd0edff32a68f7c5e683fc5dedcc4f682
UJ0 <ygl0118@gmail.com>
2021-11-14 17:14:41 +0800

    [Travel] feat: search page

--- src/views/Travel.vue
+++ src/views/Travel.vue
@@ -4,15 +4,56 @@
       <div class="row">
         <div class="col-6"></div>
         <div class="col-6">
-          <div class="flex items-center q-gutter-md">
-            <img src="@/assets/logo/travel.svg" />
-            <a href="#">
-              <img src="@/assets/icon/collection.svg" />
-            </a>
-          </div>
-          <div class="flex">
-            <q-input outlined v-model="ph" label="Label" placeholder="Placeholder" dense />
-            <q-input outlined v-model="ph" label="Label" placeholder="Placeholder" dense />
+          <div class="row q-col-gutter-md" style="width:586px">
+            <div class="col-12">
+              <div class="flex items-center q-gutter-md">
+                <img src="@/assets/logo/travel.svg" width="506"/>
+                <a href="#">
+                  <img src="@/assets/icon/collection.svg" />
+                </a>
+              </div>
+            </div>
+            <div class="col-4">
+              <q-input outlined v-model="ph" label="Label" placeholder="Placeholder" dense />
+            </div>
+            <div class="col-8">
+              <q-input outlined v-model="ph" label="Label" placeholder="Placeholder" dense />
+            </div>
+            <div class="col-4">
+              <q-card class="my-card">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+              </q-card>
+            </div>
+            <div class="col-4">
+              <q-card class="my-card">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+              </q-card>
+            </div>
+            <div class="col-4">
+              <q-card class="my-card">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+              </q-card>
+            </div>
+            <div class="col-4">
+              <q-card class="my-card">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+              </q-card>
+            </div>
+            <div class="col-4">
+              <q-card class="my-card">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+              </q-card>
+            </div>
+            <div class="col-4">
+              <q-card class="my-card">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+              </q-card>
+            </div>
+            <div class="col-12">
+              <div class="flex justify-center">
+                <img src="@/assets/icon/suggest.svg" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
