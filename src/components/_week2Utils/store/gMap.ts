119d83686b967e465351d433d9b564e53400e3c9
limitimil <zlsh10340@gmail.com>
2021-11-17 05:41:39 -0800

    STORE: g map:

---
+++ src/components/_week2Utils/store/gMap.ts
@@ -0,0 +1,47 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    map: undefined,
+  },
+  mutations: {
+    setMap(state, map) {
+      state.map = map
+    },
+  },
+  actions: {
+    initMap(context, elementId) {
+      const map = new google.maps.Map(document.getElementById(elementId), {
+        zoom: 11,
+        center: { lat: 25.03357704438537, lng: 121.56165724984085 },
+        mapTypeControl: false,
+        fullscreenControl: false,
+        zoomControl: false,
+        streetViewControl: false,
+      })
+      context.commit('setMap', map)
+    },
+    centerByMyLocation(context) {
+      const map = context.state.map
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+
+            map.setCenter(pos);
+          },
+          (err) => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        console.err('browser don\'t support location');
+      }
+    },
+  },
+  modules: {
+  },
+})
