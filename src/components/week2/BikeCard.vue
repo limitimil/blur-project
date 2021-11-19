3e3bc12537fd9aa9793923c4a5ad21f743e4d3d6
UJ0 <ygl0118@gmail.com>
2021-11-20 04:23:17 +0800

    [BikeCard] ui: stylish

--- src/components/week2/BikeCard.vue
+++ src/components/week2/BikeCard.vue
@@ -1,12 +1,21 @@
 <template>
   <q-card class="card">
-    <q-card-section>
-      <div class="text-h6">Our Changing Planet</div>
-      <div class="text-subtitle2">by John Doe</div>
-    </q-card-section>
-
-    <q-card-section class="q-pt-none">
-      <q-btn flat round color="red" icon="favorite" />
+    <q-card-section class="justify-between" horizontal>
+      <q-card-section class="column justify-between" style="width:inherit">
+        <div class="q-gutter-xs">
+          <div class="text-youbike">YouBike 1.0</div>
+          <div class="text-name">中正公園(中美路)</div>
+          <div class="text-location">中美路101-113號對面人行道</div>
+        </div>
+        <div class="row justify-center">
+          <div></div>
+          <div class="break-line"></div>
+          <div></div>
+        </div>
+      </q-card-section>
+      <q-card-actions vertical class="justify-end">
+        <q-btn flat round color="red" icon="favorite" />
+      </q-card-actions>
     </q-card-section>
   </q-card>
 </template>
@@ -23,9 +32,35 @@ export default defineComponent({
   },
 })
 </script>
-<style>
+<style lang="less">
 .card {
   width: 335px;
   height: 160px;
+  > div {
+    width: inherit;
+    height: inherit;
+  }
+}
+.text-youbike {
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.02em;
+  color: #4E4E4E;
+}
+.text-name {
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 25px;
+  letter-spacing: 0.02em;
+}
+.text-location {
+  font-size: 13px;
+  line-height: 18px;
+  color: #000000;
+}
+.break-line {
+  width: 20px;
+  border: 1px solid #D2D2D2;
+  transform: rotate(90deg);
 }
 </style>
