59bdeb4daa1a483d4757098e0539929fe2a0d407
UJ0 <ygl0118@gmail.com>
2021-11-19 20:08:28 +0800

    [Bike] feat: new page

---
+++ src/views/Bike.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page>
+    <div id="bike">
+      <div class="row">
+        <div class="col-12">
+          <q-card class="bike-card">
+            123
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  name: 'Bike',
+  components: {
+  },
+  props: {
+  },
+})
+</script>
+<style>
+.bike-card {
+  width: 335px;
+  height: 160px;
+}
+</style>
