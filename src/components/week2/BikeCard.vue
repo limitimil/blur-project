7b9d276aa5f8180af1a86948319cefa545bbbc4b
UJ0 <ygl0118@gmail.com>
2021-11-20 04:39:14 +0800

    [BikeCard] ui: grid

--- src/components/week2/BikeCard.vue
+++ src/components/week2/BikeCard.vue
@@ -1,16 +1,24 @@
 <template>
   <q-card class="card">
     <q-card-section class="justify-between" horizontal>
-      <q-card-section class="column justify-between" style="width:inherit">
+      <q-card-section class="column justify-between q-pa-sm" style="width:inherit">
         <div class="q-gutter-xs">
           <div class="text-youbike">YouBike 1.0</div>
           <div class="text-name">中正公園(中美路)</div>
           <div class="text-location">中美路101-113號對面人行道</div>
         </div>
-        <div class="row justify-center">
-          <div></div>
-          <div class="break-line"></div>
-          <div></div>
+        <div class="row justify-evenly">
+          <div class="column items-center">
+            <div class="text-available">可借車輛</div>
+            <div class="text-count">12</div>
+          </div>
+          <div class="row items-center">
+            <div class="break-line"></div>
+          </div>
+          <div class="column items-center">
+            <div class="text-available">可還車位</div>
+            <div class="text-count">08</div>
+          </div>
         </div>
       </q-card-section>
       <q-card-actions vertical class="justify-end">
@@ -36,6 +44,7 @@ export default defineComponent({
 .card {
   width: 335px;
   height: 160px;
+  border-radius: 10px !important;
   > div {
     width: inherit;
     height: inherit;
@@ -56,10 +65,19 @@ export default defineComponent({
 .text-location {
   font-size: 13px;
   line-height: 18px;
-  color: #000000;
+}
+.text-available {
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.02em;
+}
+.text-count {
+  font-size: 36px;
+  line-height: 42px;
 }
 .break-line {
-  width: 20px;
+  width: 28px;
+  height: 0px;
   border: 1px solid #D2D2D2;
   transform: rotate(90deg);
 }
