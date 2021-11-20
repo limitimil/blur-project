b5b6dde48339442f39bb6dded3faffe976e9d350
limitimil <zlsh10340@gmail.com>
2021-11-19 19:32:43 -0800

    3 files: remove unused

--- src/components/componentStories/stories/GoogleMap.vue
+++ src/components/componentStories/stories/GoogleMap.vue
@@ -39,20 +39,3 @@ export default {
 }
 
 </script>
-<style scoped lang="less">
-.hack1{
- background-color: hsla(208, 26%, 75%, 1);
-}
-.hack2{
- background-color: hsla(17, 25%, 33%, 1);
-}
-.hack3{
- background-color: hsla(203, 16%, 96%, 1);
-}
-.hack4{
- background-color: hsla(214, 15%, 62%, 1);
-}
-.hack5{
- background-color: hsla(240, 7%, 47%, 1);
-}
-</style>
