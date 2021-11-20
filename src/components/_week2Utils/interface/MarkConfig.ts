7444a8fe1f282dac05f04882fd1d334d721e0bc6
limitimil <zlsh10340@gmail.com>
2021-11-20 15:45:32 +0800

    INTERFACE: 2 files, markconfig and bikestation

---
+++ src/components/_week2Utils/interface/MarkConfig.ts
@@ -0,0 +1,10 @@
+enum AvailableType{
+  RENT,
+  RETURN,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default interface MarkConfig {
+  availableType: AvailableType
+}
+
