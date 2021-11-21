520e788d7100c7590f2cc02f8767f600408b108a
limitimil <zlsh10340@gmail.com>
2021-11-21 07:36:54 -0800

    View 2 files: set ui overlay on google map

--- src/views/SmileBikeWorkflow.vue
+++ src/views/SmileBikeWorkflow.vue
@@ -1,14 +1,32 @@
 <template>
-  <div> Hello World </div>
-  <router-view></router-view>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title class="q-py-sm">
+          <img src="@/assets/logo/smileBike.svg">
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="hack1">
+      <q-page>
+        <GMap class="filled-google-map"></GMap>
+        <router-view class="overlay-google-map"/>
+      </q-page>
+    </q-page-container>
+
+  </q-layout>
 </template>
 <script lang="ts">
 import { ref } from 'vue'
+import GMap from '@/components/_week2Utils/gMap.vue'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
+    GMap,
   },
 
   setup() {
@@ -20,6 +38,14 @@ export default {
 
 </script>
 <style scoped lang="less">
+.filled-google-map {
+  position: absolute;
+  height: 100%;
+}
+.overlay-google-map {
+  z-index: 1000003;
+  position: absolute;
+}
 .hack1{
  background-color: hsla(208, 26%, 75%, 1);
 }
