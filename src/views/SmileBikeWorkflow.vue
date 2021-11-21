8f9f5270c6b5eef49e06f7feefbcba02bbcddddc
limitimil <zlsh10340@gmail.com>
2021-11-21 06:46:48 -0800

    many files: regist router and drawer for week2 pages

---
+++ src/views/SmileBikeWorkflow.vue
@@ -0,0 +1,40 @@
+<template>
+  <div> Hello World </div>
+  <router-view></router-view>
+</template>
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+  },
+
+  setup() {
+    return {
+      leftDrawerOpen: ref(false),
+    }
+  },
+}
+
+</script>
+<style scoped lang="less">
+.hack1{
+ background-color: hsla(208, 26%, 75%, 1);
+}
+.hack2{
+ background-color: hsla(17, 25%, 33%, 1);
+}
+.hack3{
+ background-color: hsla(203, 16%, 96%, 1);
+}
+.hack4{
+ background-color: hsla(214, 15%, 62%, 1);
+}
+.hack5{
+ background-color: hsla(240, 7%, 47%, 1);
+}
+</style>
+
+
