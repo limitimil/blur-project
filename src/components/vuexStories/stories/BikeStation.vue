a9cc2267af9ec31462a787a14e788a5017b7e646
limitimil <zlsh10340@gmail.com>
2021-11-20 21:46:04 -0800

    V Story BS: search flow

--- src/components/vuexStories/stories/BikeStation.vue
+++ src/components/vuexStories/stories/BikeStation.vue
@@ -25,18 +25,26 @@
     <div class="col"><q-btn @click="calculateCenter">Calculate Center</q-btn></div>
     <div class="col">Center: {{position}}</div>
   </div>
+  <div class="row">
+    <q-btn @click="top">Search</q-btn>
+  </div>
   <GMap></GMap>
+  <div v-for="item in data" :key="item.ID">
+    <Card :value="item" />
+  </div>
 </template>
 <script lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import GMap from '@/components/_week2Utils/gMap.vue'
 import gMapStore from '@/components/_week2Utils/store/gMap'
 import bikeStationStore from '@/store/bikeStation'
+import Card from '@/components/_storyUtils/Card.vue'
 
 export default {
   name: 'GoogleMap',
   components: {
     GMap,
+    Card,
   },
   setup() {
     const position = ref(undefined)
@@ -48,25 +56,31 @@ export default {
       position.value = gMapStore.getters.map.getCenter()
     })
     return {
+      // Store
+      bikeStationStore,
+
+      // Varaibles
       usePosition,
       keyword: ref(null),
       city: ref(null),
       position,
+      data: computed(() => bikeStationStore.getters.content),
 
       // methods
       toMyLocation,
       calculateCenter,
+      top: () => bikeStationStore.dispatch('top'),
 
       // method group
       updateQueryMethods: {
         city: (value: string) => bikeStationStore.commit('appendQuery', { city: value }),
         keyword: (value: string) => bikeStationStore.commit('appendQuery', { keyword: value }),
         usePosition: (value: any) => {
-          if(value){
+          if (value) {
             calculateCenter()
-            bikeStationStore.commit('appendQuery', { position })
+            bikeStationStore.commit('appendQuery', { position: position.value })
           } else {
-            const query = bikeStationStore.getters.query
+            const { query } = bikeStationStore.getters
             delete query.nearBy
             bikeStationStore.commit('setQuery', { ...query })
           }
