da9d5e454cb144b2ce1ce5f026c77a2ec1e9a19b
UJ0 <ygl0118@gmail.com>
2021-11-22 10:50:45 +0800

    [Bike] feat: tune workflow

--- src/views/Bike.vue
+++ src/views/Bike.vue
@@ -6,11 +6,11 @@
           <div class="logo flex justify-center">
             <img src="@/assets/logo/bike.svg" width="480"/>
           </div>
-          <div class="flex justify-center">
-            <q-select v-model="city" :options="['台北市']" label="所在城市" />
-            <q-select v-model="township" :options="['中山區']" label="所在區域" />
+          <div class="flex justify-center q-gutter-md">
+            <q-select v-model="city" :options="['台北市']" label="所在城市" style="width:210px"/>
+            <q-select v-model="township" :options="['中山區']" label="所在區域" style="width:210px"/>
           </div>
-          <div class="flex justify-evenly">
+          <div class="flex justify-evenly" style="margin-top:300px">
             <img src="@/assets/icon/station.svg" width="150"/>
             <img src="@/assets/icon/road.svg" width="150"/>
             <img src="@/assets/icon/view.svg" width="150"/>
@@ -23,9 +23,10 @@
 
 <script lang="ts">
 import {
-  defineComponent, defineAsyncComponent, ref, computed,
+  defineComponent, defineAsyncComponent, ref, watchEffect,
 } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'Bike',
@@ -34,6 +35,13 @@ export default defineComponent({
   setup() {
     const city = ref('')
     const township = ref('')
+    const router = useRouter()
+    watchEffect(() => {
+      if (city.value !== '' && township.value !== '') {
+        router.push({ name: 'SmileBikeWorkflow' })
+      }
+      return { city, township }
+    })
 
     return {
       city,
