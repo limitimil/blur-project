884f41b3811de4af4962c4a070c248534caae1cb
limitimil <zlsh10340@gmail.com>
2021-11-21 18:29:03 -0800

    V Smile Bike Workflow: remove unused css

--- src/views/SmileBikeWorkflow.vue
+++ src/views/SmileBikeWorkflow.vue
@@ -9,7 +9,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-page-container class="hack1">
+    <q-page-container>
       <q-page>
         <GMap class="filled-google-map"></GMap>
         <router-view class="overlay-google-map"/>
@@ -39,20 +39,5 @@ export default {
 .filled-google-map {
   position: absolute;
   height: 100%;
-}
-.hack1{
- background-color: hsla(208, 26%, 75%, 1);
-}
-.hack2{
- background-color: hsla(17, 25%, 33%, 1);
-}
-.hack3{
- background-color: hsla(203, 16%, 96%, 1);
-}
-.hack4{
- background-color: hsla(214, 15%, 62%, 1);
-}
-.hack5{
- background-color: hsla(240, 7%, 47%, 1);
 }
 </style>
