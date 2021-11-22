b074389695dc7e1c1f51aa796f1fde82faa95316
limitimil <zlsh10340@gmail.com>
2021-11-22 07:12:58 -0800

    DF Tourism: extend TdxRequestBuilder

--- src/data-fetch/tourism.ts
+++ src/data-fetch/tourism.ts
@@ -3,64 +3,24 @@ import lodash from 'lodash'
 import axios from '@/axios'
 import TdxArgument from './interface/TdxArgument'
 import { TdxFilter, TdxStatements } from './tdx/filterDSL'
+import TdxRequestBuilder from './tdx/requestBuilder'
 
-const KEY_FOR_ID = 'ID'
-const FILEDS_FOR_KEYWORD = [
-  'Name',
-  'DescriptionDetail',
-  'Description',
-  'Address',
-  'Class1',
-  'Class2',
-  'Class3',
-  'Level',
-  'Remarks',
-  'Keyword',
-  'City',
-]
-export default class Tourism {
-  private arg: TdxArgument = {}
+export default class Tourism extends TdxRequestBuilder {
+  protected KEY_FOR_ID: string = 'ID';
 
-  private city: string | undefined = undefined;
-
-  private className: string | undefined = undefined;
-
-  private keyword: string | undefined = undefined;
-
-  private subset: string[] = [];
-
-  private filter: TdxFilter;
-
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-  constructor() {
-    this.filter = new TdxFilter()
-  }
-
-  // TODO: this builder is not able to collaberate with withKeyword, withClassName
-  public withSubset(subset: string[]) :Tourism {
-    this.filter.regist(TdxStatements.filterIdSubset(subset, KEY_FOR_ID))
-    return this
-  }
-
-  public withArg(arg: TdxArgument): Tourism {
-    this.arg = { ...arg }
-    return this
-  }
-
-  public withCity(city: string): Tourism {
-    this.city = city
-    return this
-  }
-
-  public withKeyword(keyword: string): Tourism {
-    this.filter.regist(TdxStatements.filterKeyword(keyword, FILEDS_FOR_KEYWORD))
-    return this
-  }
-
-  public withClassName(className: string): Tourism {
-    this.filter.regist(TdxStatements.filterClassName(className))
-    return this
-  }
+  protected FIELDS_FOR_KEYWORD: string[] = [
+    'Name',
+    'DescriptionDetail',
+    'Description',
+    'Address',
+    'Class1',
+    'Class2',
+    'Class3',
+    'Level',
+    'Remarks',
+    'Keyword',
+    'City',
+  ];
 
   public async invoke(): Promise<any> {
     if (this.filter.getLength()) {
@@ -94,95 +54,5 @@ export default class Tourism {
     }
     const res: AxiosResponse<any> = await axios.get(url, { params })
     return res.data
-  }
-
-  public async getScenicSpotByKeywordCity(
-    arg: TdxArgument, keyword: string, city: string,
-  ): Promise<any> {
-    const fields = [
-      'Name',
-      'DescriptionDetail',
-      'Description',
-      'Address',
-      'Class1',
-      'Class2',
-      'Class3',
-      'Level',
-      'Remarks',
-      'Keyword',
-      'City',
-    ]
-    const newArg = {
-      ...arg,
-      ...this.fieldsByKeyword(keyword, fields),
-    }
-    return this.getScenicSpotByCity(newArg, city)
-  }
-
-  public async getScenicSpotByKeyword(arg: TdxArgument, keyword: string): Promise<any> {
-    const fields = [
-      'Name',
-      'DescriptionDetail',
-      'Description',
-      'Address',
-      'Class1',
-      'Class2',
-      'Class3',
-      'Level',
-      'Remarks',
-      'Keyword',
-      'City',
-    ]
-    const newArg = {
-      ...arg,
-      ...this.fieldsByKeyword(keyword, fields),
-    }
-    return this.getScenicSpot(newArg)
-  }
-
-  // TODO: consider city, keyword, classname
-  public async getScenicSpotByClassName(arg: TdxArgument, className: string): Promise<any> {
-    const newArg = {
-      ...arg,
-      ...this.byClassName(className),
-    }
-    return this.getScenicSpot(newArg)
-  }
-
-  // TODO: find a better place to reside these methods
-  public byClassName(className: string): TdxArgument {
-    const getKeywordBlock = (field: string) => `${field} eq '${className}'`
-    const filterStr = `${getKeywordBlock('Class1')} or ${getKeywordBlock('Class2')} or ${getKeywordBlock('Class3')}`
-    return {
-      $filter: this.arg.$filter ? `${this.arg.$filter} and ${filterStr}` : filterStr,
-    }
-  }
-
-  public fieldsByKeyword(keyword: string, fields: string[]): TdxArgument {
-    const getKeywordBlock = (field: string) => `contains(${field}, '${keyword}')`
-    if (fields.length === 0) {
-      return {}
-    }
-    const firstFilterStr = getKeywordBlock(fields[0])
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const filterStr = lodash.reduce(fields.slice(1), (result, value, key) => `${result} or ${getKeywordBlock(value)}`, firstFilterStr)
-    return {
-      $filter: this.arg.$filter ? `${this.arg.$filter} and ${filterStr}` : filterStr,
-    }
-  }
-
-  public bySubset(subset: string[]) :TdxArgument {
-    const getKeywordBlock = (key: string) => `ID eq '${key}'`
-    if (subset.length === 0) {
-      return {}
-    }
-    const firstFilterStr = getKeywordBlock(subset[0])
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const filterStr = lodash.reduce(subset.slice(1), (result, value, key) => `${result} or ${getKeywordBlock(value)}`, firstFilterStr)
-    return {
-      $filter: filterStr,
-    }
   }
 }
