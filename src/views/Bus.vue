34290f2bfefe916219214594e19455d8163ef043
UJ0 <ygl0118@gmail.com>
2021-11-22 17:30:09 +0800

    [Bus] feat: main page

--- src/views/Bus.vue
+++ src/views/Bus.vue
@@ -3,17 +3,21 @@
     <div id="bus">
       <div class="row">
         <div class="col-12">
-          <div class="logo column items-center">
+          <div class="column justify-evenly items-center" style="height:990px">
             <img src="@/assets/logo/bus-en.svg" width="372"/>
             <img src="@/assets/logo/bus-zh.svg" width="253"/>
-          </div>
-          <div class="flex justify-center q-gutter-md">
-            <q-select v-model="city" :options="['台北市']" label="所在城市" style="width:210px"/>
-            <q-select v-model="township" :options="['中山區']" label="所在區域" style="width:210px"/>
+            <q-select v-model="city" :options="['台北市']" label="選擇縣市" style="width:446px"/>
+            <q-btn color="amber" label="START" size="30px"/>
           </div>
         </div>
       </div>
     </div>
+
+    <q-footer class="text-white">
+      <div class="flex flex-center" style="height:inherit">
+        © BLUR 2021 │ 六角學院 │ TDX運輸資料流通服務
+      </div>
+    </q-footer>
   </q-page>
 </template>
 
@@ -25,29 +29,24 @@ import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 
 export default defineComponent({
-  name: 'Bike',
+  name: 'Bus',
   components: {
-  },
-  setup() {
-    const city = ref('')
-    const township = ref('')
-    const router = useRouter()
-    watchEffect(() => {
-      if (city.value !== '' && township.value !== '') {
-        router.push({ name: 'BikeStation' })
-      }
-      return { city, township }
-    })
-
-    return {
-      city,
-      township,
-    }
   },
 })
 </script>
 <style lang="less">
+html {
+  background: #E5E8E7;
+}
+#bus {
+  height: 100%;
+}
 .logo {
   margin-top: 120px;
+}
+.q-footer {
+  height: 90px;
+  background: #747474;
+  font-size: 18px;
 }
 </style>
