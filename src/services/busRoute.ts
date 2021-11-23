91ca1f33ad5eec3dab023f032415fa6ec58accf4
limitimil <zlsh10340@gmail.com>
2021-11-23 05:53:59 -0800

    SERVICE: bufix

--- src/services/busRoute.ts
+++ src/services/busRoute.ts
@@ -4,6 +4,20 @@ import FetchableService from './fetchableService'
 export default class BusRouteService extends FetchableService {
   protected builderClass: any = BusRoute;
 
+  // TODO: try to extract this method to FetchableService
+  public async getAll(): Promise<any> {
+    const arg = {}
+    // eslint-disable-next-line new-cap
+    let builder = new this.builderClass().withArg(arg)
+    if (this.city) {
+      builder = builder.withCity(this.city)
+    }
+    if (this.keyword) {
+      builder = builder.withKeyword(this.keyword)
+    }
+    return builder.invoke()
+  }
+
   public async getByUniqId(id: string, key:string = 'RouteID') {
     const arg = { $top: 1 }
     // eslint-disable-next-line new-cap
