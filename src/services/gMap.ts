cf6863b71787f2b20183bef98d9a98f17736818e
limitimil <zlsh10340@gmail.com>
2021-11-24 07:56:32 -0800

    SERVICE GMAP: remove console log

--- src/services/gMap.ts
+++ src/services/gMap.ts
@@ -4,11 +4,8 @@ import { BusDataType, getBusData } from '@/data-fetch/bus'
 // TODO: this handler should be refactored as an action of some store
 // eslint-disable-next-line import/prefer-default-export
 export async function drawBusStopOnMap(city: string, routeName: string, direction: string | number, gMapStore: any) {
-  console.log(routeName)
   const stopCollection = await getBusData(city, BusDataType.DisplayStopOfRoute, routeName)
-  console.log(stopCollection)
   const selectedDirection = stopCollection[parseInt(direction.toString(), 10)]
-  console.log(selectedDirection)
   const { Stops } = selectedDirection
   Stops.forEach((element: BusStop) => {
     gMapStore.dispatch('markTdxPosition', element.StopPosition)
