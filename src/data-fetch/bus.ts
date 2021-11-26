c7062d3ba57795bc22c7993cd8330d541c974817
limitimil <zlsh10340@gmail.com>
2021-11-26 09:26:13 -0800

    DF Bus:
    DF Bus: include stop of route, which is more robust
    DF Bus: builder pattern for Bus data fetcher

--- src/data-fetch/bus.ts
+++ src/data-fetch/bus.ts
@@ -1,12 +1,14 @@
 import { AxiosResponse } from 'axios'
 import axios from '@/axios'
+import TdxArgument from './tdx/interface/TdxArgument'
 
 // eslint-disable-next-line no-shadow
 export enum BusDataType {
   RealTimeByFrequency= 'RealTimeByFrequency',
   RealTimeNearStop= 'RealTimeNearStop',
   EstimatedTimeOfArrival= 'EstimatedTimeOfArrival',
-  DisplayStopOfRoute= 'DisplayStopOfRoute',
+  DisplayStopOfRoute= 'DisplayStopOfRoute', // We should avoid using this.
+  StopOfRoute= 'StopOfRoute',
 }
 
 export async function getBusData(city: string, type: BusDataType, routeName: string): Promise<any[]> {
@@ -24,4 +26,64 @@ export async function getBusDataStreaming(city: string, type: BusDataType, route
   }
   const res: AxiosResponse<any> = await axios.get(url, { params })
   return res.data
+}
+
+// @ts-ignore
+function insert(value, index, item) {
+    value.splice( index, 0, item );
+}
+
+// TODO: need ut to guard magic of urlPath
+export default class Bus {
+  private arg: TdxArgument = {}
+
+  private urlPath: string[] = [
+    'MOTC',
+    'v2',
+    'Bus',
+    'City',
+  ]
+
+  public withDirection(direction: number): Bus {
+    this.arg = {
+      ...this.arg,
+      $filter: `direction eq ${direction}`,
+    }
+    return this
+  }
+
+  public useStreaming(): Bus {
+    const idx = this.urlPath.findIndex((elem) => elem === 'Bus')
+    insert(this.urlPath, idx+1, 'Streaming')
+    return this
+  }
+
+  public withRouteName(routeName: string): Bus {
+    const idx = this.urlPath.length
+    insert(this.urlPath, idx, routeName)
+    return this
+  }
+
+  private insertCity(city: string): void{
+    const idx = this.urlPath.findIndex((elem) => elem === 'City')
+    insert(this.urlPath, idx+1, city)
+  }
+
+  private insertType(type: string): void{
+    const idx = this.urlPath.findIndex((elem) => elem === 'Bus')
+    insert(this.urlPath, idx+1, type)
+  }
+
+  public async invoke(city: string, type: BusDataType): Promise<any[]> {
+    this.insertCity(city)
+    this.insertType(type)
+    const url = this.urlPath.join('/')
+    const params = {
+      $format: 'JSON',
+      ...this.arg,
+    }
+
+    const res: AxiosResponse<any> = await axios.get(url, { params })
+    return res.data
+  }
 }
