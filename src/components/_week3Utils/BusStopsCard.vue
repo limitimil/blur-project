4fa5c8da6a106f2b56681c254d6f1c3ceac69a6f
UJ0 <ygl0118@gmail.com>
2021-11-29 10:30:05 +0800

    fix: minor

--- src/components/_week3Utils/BusStopsCard.vue
+++ src/components/_week3Utils/BusStopsCard.vue
@@ -12,18 +12,18 @@
     </q-card-section>
     <q-card-section>
       <div class="row q-col-gutter-sm">
-        <div class="col-3 text-title">站序</div>
+        <div class="col-3 text-title flex justify-center">站序</div>
         <div class="col-6 text-title">站名</div>
-        <div class="col-3 text-title">預估到站</div>
+        <div class="col-3 text-title flex justify-center">預估到站</div>
         <template v-for="(stop, index) in value.Stops" :key="index">
-          <div class="col-3 text-content">{{stop.StopSequence}}</div>
+          <div class="col-3 text-content flex justify-center">{{stop.StopSequence}}</div>
           <div class="col-6 text-content">{{stop.StopNameZh}}</div>
-          <div v-if="stop.EstimateTime > 60" class="col-3 text-content">{{new Date(stop.EstimateArrivalTimeStamp * 1000).toString().slice(16, 21).replace('T', ' ')}}</div>
-          <div v-else-if="stop.StopStatus === 0" class="col-3 text-content">即將進站</div>
-          <div v-else-if="stop.StopStatus === 1" class="col-3 text-content">尚未發車</div>
-          <div v-else-if="stop.StopStatus === 2" class="col-3 text-content">交管不停靠</div>
-          <div v-else-if="stop.StopStatus === 3" class="col-3 text-content">末班車已過</div>
-          <div v-else-if="stop.StopStatus === 4" class="col-3 text-content">今日未營運</div>
+          <div v-if="stop.EstimateTime > 60" class="col-3 text-content flex justify-center">{{new Date(stop.EstimateArrivalTimeStamp * 1000).toString().slice(16, 21).replace('T', ' ')}}</div>
+          <div v-else-if="stop.StopStatus === 0" class="col-3 text-content flex justify-center">即將進站</div>
+          <div v-else-if="stop.StopStatus === 1" class="col-3 text-content flex justify-center">尚未發車</div>
+          <div v-else-if="stop.StopStatus === 2" class="col-3 text-content flex justify-center">交管不停靠</div>
+          <div v-else-if="stop.StopStatus === 3" class="col-3 text-content flex justify-center">末班車已過</div>
+          <div v-else-if="stop.StopStatus === 4" class="col-3 text-content flex justify-center">今日未營運</div>
         </template>
       </div>
     </q-card-section>
